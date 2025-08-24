022ad3df32577eabf8071786b103bf070880494d
DaltJM <56285459+DaltJM@users.noreply.github.com>
2025-08-24 11:33:05 +1000

    init props

--- bin/demo-s3-cdk.ts
+++ bin/demo-s3-cdk.ts
@@ -1,20 +1,13 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import { DemoS3CdkStack } from '../lib/demo-s3-cdk-stack';
+import * as s3 from 'aws-cdk-lib/aws-s3'
 
 const app = new cdk.App();
 new DemoS3CdkStack(app, 'DemoS3CdkStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+s3BucketName: "demo-bucket",
+s3BlockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+s3Encryption: s3.BucketEncryption.S3_MANAGED,
+s3RemovalPolicy: cdk.RemovalPolicy.RETAIN,
+s3Versioning: false,
 });
--- lib/demo-s3-cdk-stack.ts
+++ lib/demo-s3-cdk-stack.ts
@@ -2,16 +2,24 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3'
 
+export interface DemoS3CdkStackProps extends cdk.StackProps {
+s3BucketName: string;
+s3BlockPublicAccess: s3.BlockPublicAccess;
+s3Encryption: s3.BucketEncryption;
+s3RemovalPolicy: cdk.RemovalPolicy
+s3Versioning: boolean;
+};
+
 export class DemoS3CdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: DemoS3CdkStackProps) {
     super(scope, id, props);
 
     const demoBucket = new s3.Bucket(this, "DemoBucket", {
-      bucketName: "demo-bucket",
-      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
-      encryption: s3.BucketEncryption.S3_MANAGED,
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
-      versioned: false,
+      bucketName: props.s3BucketName,
+      blockPublicAccess: props.s3BlockPublicAccess,
+      encryption: props.s3Encryption,
+      removalPolicy: props.s3RemovalPolicy,
+      versioned: props.s3Versioning,
     })
   }
 }
